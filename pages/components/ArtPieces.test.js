5602018616c4e7982df9b7d0390e44d4f1bd7c08
ALAN TONELLO <alantb2@gmail.com>
2024-02-05 14:41:30 +0100

    changed components folder structure

--- pages/components/ArtPieces.test.js
+++
@@ -1,8 +0,0 @@
-// import { render, screen } from "@testing-library/react";
-// import ArtPieces from "./ArtPieces";
-
-// test("renders an unordered list of art pieces", () => {
-//   render(<ArtPieces />);
-//   const unorderedList = screen.getByRole("list");
-//   expect(unorderedList).toBeInTheDocument();
-// });
