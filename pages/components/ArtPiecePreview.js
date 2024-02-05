a741e2d998122dd2d67e28e6fbf628aa6080c192
furmandarya1@gmail.com <furmandarya1@gmail.com>
2024-02-05 13:31:01 +0100

    fix error

--- pages/components/ArtPiecePreview.js
+++ pages/components/ArtPiecePreview.js
@@ -2,7 +2,7 @@ export default function ArtPiecePreview({ title, artist, image }) {
   return (
     <>
       <h1>{title}</h1>
-      <img src={image} alt={title} />
+      <img src={image} alt={title} width={500} height={300} />
       <p>Artist: {artist}</p>
     </>
   );
