fef5206000ad75d0ed5f4661f23476dc2942a0cd
ALAN TONELLO <alantb2@gmail.com>
2024-02-05 17:03:30 +0100

    implemented testing

---
+++ components/Spotlight.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Spotlight from "./Spotlight";
+
+test("renders a randomly picked art piece image with its artist name", () => {
+  render(<Spotlight />);
+  const heading = screen.getByRole("heading");
+  const image = screen.getByRole("img");
+  const paragraph = screen.getByText("Artist:");
+  expect(heading).toBeInTheDocument();
+  expect(image).toBeInTheDocument();
+  expect(paragraph).toBeInTheDocument();
+});
