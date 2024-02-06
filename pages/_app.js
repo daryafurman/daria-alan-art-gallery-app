f3f971dd042c9c36b4914433c60bc2a46fe174ca
ALAN TONELLO <alantb2@gmail.com>
2024-02-06 16:09:36 +0100

    updated variables/immer

--- pages/_app.js
+++ pages/_app.js
@@ -72,7 +72,7 @@ export default function App({ Component, pageProps }) {
         <Layout>
           <Component
             {...pageProps}
-            artPiecesInfo={artPiecesInfo}
+            artPiecesInfo={artPieces}
             onToggleFavorite={handleToggleFavorite}
           />
         </Layout>
--- pages/art-pieces/[slug]/index.js
+++ pages/art-pieces/[slug]/index.js
@@ -1,12 +1,15 @@
 import { useRouter } from "next/router";
 import ArtPieceDetails from "@/components/ArtPieceDetails";
 
-export default function ArtPieceDetailsPage({ artPieces }) {
+export default function ArtPieceDetailsPage({ artPiecesInfo }) {
   const router = useRouter();
   const { slug } = router.query;
 
+  console.log(artPiecesInfo);
   // Find the art piece based on the slug
-  const selectedArtPiece = artPieces.find((artPiece) => artPiece.slug === slug);
+  const selectedArtPiece = artPiecesInfo.find(
+    (artPiece) => artPiece.slug === slug
+  );
 
   if (!selectedArtPiece) {
     // Handle case where art piece is not found
